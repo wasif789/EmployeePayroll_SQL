8508d2bb42d2be12ff12c8bbcaf2203ca959be37
wasif789 <wsffarid1@gmail.com>
2021-11-26 17:23:20 +0530

    Insert query to add data in table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -20,3 +20,8 @@ emailId varchar(20)
 );
 
 --------------------------------------
+-------------UC3 Inserting the values in table-----------
+
+Insert into employee_payroll values ('Ashok',70000,'2021-05-15','ashok32@gmail.com'),('Karthick',55000,'2021-02-13','karthi21@gmail.com'),('Gayatri',45000,'2021-07-13','gaya@gmail.com'),('Divya',30000,'2021-08-23','divya2000@gmail.com');
+
+-----------------------------------------------
