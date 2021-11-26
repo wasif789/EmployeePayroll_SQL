703d36ed5e67c2a68722b40efd656023ad3b5426
wasif789 <wsffarid1@gmail.com>
2021-11-26 17:18:54 +0530

    Create table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -6,3 +6,17 @@ create database payroll_services;
 
 use payroll_services;
 ----------------------------------
+--------------UC2--------------
+
+--------Create Tabel------------
+
+create table employee_payroll
+(
+empId int identity(1,1) primary key,
+name varchar(20) not null,
+salary float,
+startDate date,
+emailId varchar(20)
+);
+
+--------------------------------------
