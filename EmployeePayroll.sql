b0319fe9a65db752dd01e9e246d882ed0b0b95da
wasif789 <wsffarid1@gmail.com>
2021-11-28 00:29:59 +0530

    er diagram updated

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -118,3 +118,92 @@ Insert into employee_payroll values('Ashok',70000,'2021-05-15','ashok32@gmail.co
 
 select * from employee_payroll; 
 ------------------------------------------------------------------
+---------------------UC11 ER Diagram---------------
+
+---Company Table
+
+Create Table Company(CompanyID int identity(1,1) primary key,CompanyName varchar(100));
+
+---Insert the values
+Insert into Company values ('Kishore'),('Kavya');
+---Retrieve the data
+select * from company;
+
+--Create Employee Table
+create table Employee
+(EmployeeID int identity(1,1) primary key,
+Company_Id int,
+EmployeeName varchar(200),
+EmployeePhoneNum bigInt,
+EmployeeAddress varchar(200),
+StartDate date,
+Gender char,
+Foreign key (Company_Id) references Company(CompanyID)
+);
+---Insert the employee table
+insert into Employee (Company_Id,EmployeeName,EmployeePhoneNum,EmployeeAddress,StartDate,Gender)values
+(1,'Vedhashni','9823567453','678 Greenwich street','2020-09-03','F'),
+(2,'Nivetha','8954785124',' 456 Fith Avenue','2021-03-12','F'),
+(1,'Kavin','9874587158','4682 Stone Street','2018-02-06','M'),
+(2,'Rajkumar','8974587197','5678 Convent Avenue','2019-01-16','M');
+
+update Employee set EmployeeName='Deepak' where EmployeePhoneNum='8974587197';
+---Retrieve the data
+Select * from employee;
+
+--Create Payroll Table
+create table PayRollCalculate
+(
+Employee_Id int,
+BasicPay float,
+TaxablePay float,
+IncomeTax float,
+NetPay float,
+Deductions float,
+Foreign key (Employee_Id) references Employee(EmployeeID)
+);
+---Insert the values in payrollcalculate table
+Insert into PayRollCalculate (Employee_Id,BasicPay,IncomeTax,Deductions)values
+(1,50000,1000,1500),
+(2,60000,1432,1700),
+(3,75000,1300,1870),
+(4,80000,3000,2300);
+---Retrieve the data
+Select * from PayRollCalculate;
+
+--Set the taxablepay and netpay value using update
+update PayRollCalculate set TaxablePay=BasicPay-Deductions;
+
+update PayRollCalculate set NetPay=TaxablePay-IncomeTax;
+
+--Create Department Table
+create table DepartmentTable
+(
+DepartmentId int identity(1,1) primary key,
+DepartName varchar(100)
+);
+--Insert the value
+Insert into DepartmentTable values
+('HR'),
+('Research development'),
+('Product development');
+--Retrieve the data
+Select * from DepartmentTable;
+
+----Create Employee Department table
+create table EmpDept
+(
+Dept_Id int ,
+Employee_Id int,
+Foreign key (Employee_Id) references Employee(EmployeeID),
+Foreign key (Dept_Id) references DepartmentTable(DepartmentID)
+);
+------Insert the values in employeedept table
+Insert into EmpDept(Dept_Id,Employee_Id) values
+(1,2),
+(2,3),
+(3,4),
+(2,1);
+--Retrieve the data
+select * from EmpDept;
+--------------------------------------------------------------------
