cc0ec875c2c611e4ae31341316cdedf882cb96b3
wasif789 <wsffarid1@gmail.com>
2021-11-28 00:13:25 +0530

    Add coloums for phone number,department,addres

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -61,3 +61,31 @@ select count(salary) as CountofGender ,gender from employee_payroll   group by g
 
 select max(salary) as maxSalary ,gender from employee_payroll   group by gender;
 ---------------------------------------------------------
+----------------------UC8 Adding phone number, department and address columns-----------------
+
+alter table employee_payroll add PhoneNumber bigint;
+
+alter table employee_payroll add Department varchar(25) not null default 'HR';
+
+update employee_payroll set PhoneNumber=8945125478 where name='Ashok';
+update employee_payroll set PhoneNumber=9845127810 where name='Karthick';
+update employee_payroll set PhoneNumber=8745123654 where name='Gayatri';
+update employee_payroll set PhoneNumber=9874512458 where name='Divya';
+
+update employee_payroll set Department='HR' where name='Ashok';
+update employee_payroll set Department='Product development' where name='Gayatri';
+update employee_payroll set Department='Business development' where name='Divya';
+update employee_payroll set Department='Research development' where name='Karthick';
+
+
+alter table employee_payroll add Address varchar(25) default 'Not Provided';
+
+
+update employee_payroll set Address='Chennai' where name='Ashok';
+update employee_payroll set Address='Bangalore' where name='Karthick';
+update employee_payroll set Address='Cochin' where name='Gayatri';
+update employee_payroll set Address='Hyderbad' where name='Divya';
+
+
+select * from employee_payroll;
+---------------------------------------------------------------------
